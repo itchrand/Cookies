677197a10b8afab596c1e5661036f50e686c3e55
itchrand <ac_is_nowhere@yahoo.com>
2017-11-07 14:20:09 +0100

    Initial commit

---
+++ app/src/main/java/it/seahawk/cookies/MainActivity.kt
@@ -0,0 +1,23 @@
+package it.seahawk.cookies
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    fun eatCookie(view: View) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+
+    }
+}
